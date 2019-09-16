ce6d761a576f1e3de6935b2248c218b0154ea7bf
Willian Viana <willian.batista.viana@gmail.com>
2019-09-16 13:29:40 -0300

    feat(init): start project and configs

---
+++ src/routes.js
@@ -0,0 +1,5 @@
+const routes = require('express').Router()
+
+// definição de rotas
+
+modules.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const app = require('./app')
+
+app.listen(proccess.env.PORT || 3000)
